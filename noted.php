ac68d67f09d01ef19d4c74c2c875c8c9eee20a03
Ogi Setiawan <ogisetaiwan21@gmail.com>
2019-12-17 14:25:28 +0700

    add noted file

---
+++ noted.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ ** Git : Versioning control system pada aplikasi.
+ */
+
+ //@ GIT FILE
+ //? MODIFIED : merubah struktural source data
+ //? STAGED : menambahakan perubahan soruce data ke dalam git local (.git)
+ //? COMMIT : memberikan nama/keternagan dalam suata perubahan
+
+// @ GIT COMMAND
+//! git init : initialize folder/soruce untuk versioning
+//! git add * :  menambahakan smua file / git add [nama_file]
+//! git remote add origin https://github.com/ogisetiawan/git-note.git : menambahakan remote repository github
+//! git push -u origin master : push soruce data ke github 
+//! git log : melihat log pada git /git log --oneline
