3099f682dea5cbb6d98152bc350a414fb2031d10
Ogi Setiawan <ogisetaiwan21@gmail.com>
2019-12-30 16:56:42 +0700

    git merge command

--- git-command.php
+++ git-command.php
@@ -1,3 +1,17 @@
+
+<?php
+/**
+ ** git : Versioning control system pada aplikasi.
+ ** git branch : fitur untuk membuat branch baru, agar mencegah konfilik pada saat commit bersama
+ **  
+ * 
+ */
+ //@ GIT FILE
+ //? MODIFIED: merubah struktural source data
+ //? STAGED  : menambahakan perubahan soruce data ke dalam git local (.git)
+ //? COMMIT  : memberikan nama/keternagan dalam suata perubahan
+
+ 
 //! git init                                                         : initialize folder/soruce untuk versioning
 //! git add *                                                        : menambahakan smua file / git add [nama_file]
 //! git commit                                                       : menbmahakan nama/keterangan pada perubahan
@@ -22,8 +36,9 @@
 //! git branch                                                       : melihat isi branch pda repo
 //! git checkout                                                     : pindah ke cabang baru utuk commit/prubahan baru
 //! git checkout [nomor_commit]                                      : melihat file dimasa lalu sesuai nomor commit
+//! git merge [nama_branch]                                          : menggabungkan branch master dgn branch lain
 //! git reset --soft [nomor_commit]                                  : mengembalikan kondisi file dalam keadaan staged
-//! git reset --mixed [nomor_commit]                                  : mengembalikan kondisi file dalam keadaan modified
+//! git reset --mixed [nomor_commit]                                 : mengembalikan kondisi file dalam keadaan modified
 //! git reset --hard [nomor_commit]                                  : mengembalikan kondisi file dalam keadaan commit
 //! git clone                                                        : membuat clone data soruce git repo, mlnjutkan/menambahkan perubahan pada app
 //! git clone --no-checkout <repo_url>                               : clone file .git / file commit terakhir .git 
