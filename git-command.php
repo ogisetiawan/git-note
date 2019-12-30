fbd7352ae89a71dfe5eb881fea694bd92f009093
Ogi Setiawan <ogisetaiwan21@gmail.com>
2019-12-30 16:35:57 +0700

    Menggunakan Percabangan untuk Mencegah Konflik #git command

--- git-command.php
+++ git-command.php
@@ -1,18 +1,26 @@
 <?php
 /**
- ** Git : Versioning control system pada aplikasi.
+ ** git : Versioning control system pada aplikasi.
+ ** git branch : fitur untuk membuat branch baru, agar mencegah konfilik pada saat commit bersama
+ **  
+ * 
  */
 
  //@ GIT FILE
- //? MODIFIED : merubah struktural source data
- //? STAGED : menambahakan perubahan soruce data ke dalam git local (.git)
- //? COMMIT : memberikan nama/keternagan dalam suata perubahan
+ //? MODIFIED: merubah struktural source data
+ //? STAGED  : menambahakan perubahan soruce data ke dalam git local (.git)
+ //? COMMIT  : memberikan nama/keternagan dalam suata perubahan
 
 // @ GIT COMMAND
-//! git init : initialize folder/soruce untuk versioning
-//! git add * :  menambahakan smua file / git add [nama_file]
-//! git commit : menbmahakan nama/keterangan pada perubahan
-//! git remote add origin https://github.com/ogisetiawan/git-note.git : menambahakan remote repository github
-//! git push -u origin master : push soruce data ke github 
-//! git log : melihat log pada git /git log --oneline
-//! git clone : membuat clone data soruce git repo, mlnjutkan/menambahkan perubahan pada app
+//! git init                                                         : initialize folder/soruce untuk versioning
+//! git add *                                                        : menambahakan smua file / git add [nama_file]
+//! git commit                                                       : menbmahakan nama/keterangan pada perubahan
+//! git remote add origin https-//github.com/ogisetiawan/git-note.git: menambahakan remote repository github
+//! git push -u origin master                                        : push soruce data ke github 
+//! git log                                                          : melihat log pada git /git log --oneline
+//! git branch [nama_branch_baru]                                    : membuat percabangan baru
+//! git branch                                                       : melihat isi branch pda repo
+//! git checkout master                                              : pindah ke cabang baru utuk commit/prubahan baru
+//! git marge master                                                 : menggabungkan branch ke branch master
+//! git clone                                                        : membuat clone data soruce git repo, mlnjutkan/menambahkan perubahan pada app
+//! git clone --no-checkout <repo_url>                               : clone file .git / file commit terakhir .git
