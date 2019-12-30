f946b8a2d46f75f00caa14253eb0ee3b59b22e7a
Ogi Setiawan <ogisetaiwan21@gmail.com>
2019-12-30 16:58:17 +0700

    set up strucrtural source master branch

--- git-command.php
+++ git-command.php
@@ -1,3 +1,23 @@
+<?php
+/**
+ ** git : Versioning control system pada aplikasi.
+ ** git branch : fitur untuk membuat branch baru, agar mencegah konfilik pada saat commit bersama
+ **  
+ * 
+ */
+ //@ GIT FILE
+ //? MODIFIED: merubah struktural source data
+ //? STAGED  : menambahakan perubahan soruce data ke dalam git local (.git)
+ //? COMMIT  : memberikan nama/keternagan dalam suata perubahan
+
+ 
+//! git init                                                         : initialize folder/soruce untuk versioning
+//! git add *                                                        : menambahakan smua file / git add [nama_file]
+//! git commit                                                       : menbmahakan nama/keterangan pada perubahan
+//! git remote add origin https                                      : //github.com/ogisetiawan/git-note.git : menambahakan remote repository github
+//! git push -u origin master                                        : push soruce data ke github 
+//! git log                                                          : melihat log pada git /git log --oneline
+//! git clone                                                        : membuat clone data soruce git repo, mlnjutkan/menambahkan perubahan pada app 
 //! git init                                                         : initialize folder/soruce untuk versioning
 //! git add *                                                        : menambahakan smua file / git add [nama_file]
 //! git commit                                                       : menbmahakan nama/keterangan pada perubahan
@@ -15,8 +35,9 @@
 //! git branch                                                       : melihat isi branch pda repo
 //! git checkout                                                     : pindah ke cabang baru utuk commit/prubahan baru
 //! git checkout [nomor_commit]                                      : melihat file dimasa lalu sesuai nomor commit
+//! git merge [nama_branch]                                          : menggabungkan branch master dgn branch lain
 //! git reset --soft [nomor_commit]                                  : mengembalikan kondisi file dalam keadaan staged
-//! git reset --mixed [nomor_commit]                                  : mengembalikan kondisi file dalam keadaan modified
+//! git reset --mixed [nomor_commit]                                 : mengembalikan kondisi file dalam keadaan modified
 //! git reset --hard [nomor_commit]                                  : mengembalikan kondisi file dalam keadaan commit
 //! git clone                                                        : membuat clone data soruce git repo, mlnjutkan/menambahkan perubahan pada app
 //! git clone --no-checkout <repo_url>                               : clone file .git / file commit terakhir .git 
