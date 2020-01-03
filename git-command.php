7e1fbbc0f8dbf71685388f561cd7c6fa91307022
Ogi Setiawan <ogisetaiwan21@gmail.com>
2020-01-03 22:39:56 +0700

    git command #remote

--- git-command.php
+++ git-command.php
@@ -2,10 +2,13 @@
 /**
  ** git            : verisoning control system pada sebuah aplikasi.
  ** git file       : proses atau keadaan file pada waktu itu.
- ** git configure  : konfigurasi awal untuk proses upload repo lokal ke repo online (git)
+ ** git remote     : remote repository lokal ke repository online
+ *?                  git push : proses mengirim repository ke repo online
+ *?                  git fetch: proses mengambil repository ke repo lokal, mengambil revisi terakhir tnpa melakukan merge ke repo lokal // berguna jika ada perubahan trkhir dari branch master tapi dilokal kita blm update perubahan terkahir, dan kita sudah mlkukan perubahan di repo online, tp blm push ke repo lokal. repo lokal kita tdak akan hilang perubahanya tapi update dgn perubahan terkahir dari master
+ *?                  git pull : proses mengambil repository ke repo lokal, mengambil revisi terkhir dan langsung melakukan penggabungan (merge) terhadap repository lokal.
+ *?                  git clone: proses mengambil repository ke repo lokal, tanpa mengambil revisi terkahir dari repository lokal // artinya repo lokal kita masih kosong dari perubahan (tidak ada file .git)
  ** git percabangan: fitur untuk membuat branch baru, agar mencegah konfilik pada saat commit bersama team.
  ** git previous   : fitur untuk melihat/merubah/menambahkan perubahan file pada waktu tertentu
- *?                  git checkout: mengembalikan file ke kondisi sebelumnya, SEMENTARA
  *?                  git reset   : mengembalikan file ke kondisi sebelumnya, kemudian menghapus catatan sejarah commit beikutnya.
  *?                  git revert  : mengembalikan file ke kondisi sebelumnya, dengan tidak menghapus catatan sejarah commit beikutnya.
  **  
@@ -17,7 +20,7 @@
  //? COMMIT  : memberikan nama/keterangan dalam suatu perubahan
  //? PUSH    : upload file/perubahan file ke repo online
 
-//@ GIT CONFIGURE  
+//@ GIT REMOTE  
 //! git init                                                         : initialize folder/soruce untuk versioning
 //! git add *                                                        : menambahakan smua file / git add [nama_file]
 //! git commit                                                       : menbmahakan nama/keterangan pada perubahan
@@ -35,8 +38,8 @@
 //! git reset --soft [nomor_commit]                                  : mengembalikan kondisi file dalam keadaan staged
 //! git reset --mixed [nomor_commit]                                 : mengembalikan kondisi file dalam keadaan modified
 //! git reset --hard [nomor_commit]                                  : mengembalikan kondisi file dalam keadaan commit
-//! git clone                                                        : membuat clone data soruce git repo, melanjutkan/menambahkan perubahan pada app terkahir
-//! git clone --no-checkout <repo_url>                               : clone file .git pada commit terakhir 
 
 // @ OTHER 
 //! git log                                                          : melihat log pada git /git log --oneline
+//! git clone                                                        : membuat clone data soruce git repo, melanjutkan/menambahkan perubahan pada app terkahir
+//! git clone --no-checkout <repo_url>                               : clone file .git pada commit terakhir 
