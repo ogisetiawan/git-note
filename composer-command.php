7e0ed5ed81efc30942cf0be83180d01118c6854b
Ogi Setiawan <ogisetaiwan21@gmail.com>
2020-01-04 20:52:14 +0700

    composer command #final

--- composer-command.php
+++ composer-command.php
@@ -12,3 +12,5 @@
 //! composer require nesbot/carbon -vvv: perintah untuk melakukan instalasi library 
 //?                                    - composer.json pd attribute required akan terisi sesuai nama libarary 
 //?                                    - selain itu akan ada kumpulan library pada folder vendor
+//! composer search [nama_lib]         : perintah untuk mencari library or packagist.org
+//! composer remove <vendor>/<library> : perintah untuk menhapus salah satu lib yang sudah terinstall
