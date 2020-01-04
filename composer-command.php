9e6801bd21c125d3a1773019e5f9ca93b293ea76
Ogi Setiawan <ogisetaiwan21@gmail.com>
2020-01-04 20:40:07 +0700

    composer command #init

--- composer-command.php
+++ composer-command.php
@@ -5,3 +5,10 @@
  *?            - running script
  *?            - testing 
  */
+
+//@ COMPOSER CONFIG
+//! composer init                      : perintah untuk melakukan instalasi pada sebuah directory
+//?                                    - perintah ini akan menghasilkan composer.json pada dir
+//! composer require nesbot/carbon -vvv: perintah untuk melakukan instalasi library 
+//?                                    - composer.json pd attribute required akan terisi sesuai nama libarary 
+//?                                    - selain itu akan ada kumpulan library pada folder vendor
