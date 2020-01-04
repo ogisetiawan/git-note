b1bcc3061467a7616b80a75597324b0c827300f7
Ogi Setiawan <ogisetaiwan21@gmail.com>
2020-01-04 20:39:31 +0700

    add file :: composer-command

--- composer-command.php
+++ composer-command.php
@@ -1,1 +1,7 @@
 <?php
+/**
+ ** composer : program untuk menejemen proyek base php  
+ *?            - mengintall library yang saling membutuhkan (depedency)
+ *?            - running script
+ *?            - testing 
+ */
