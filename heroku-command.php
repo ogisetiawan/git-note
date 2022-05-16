6807f900f72c14bc8d79759e48224b1be23ff501
ogisetiawan <ogisetiawan21@gmail.com>
2022-05-16 23:39:26 +0700

    feat: Add Heroku CLI Deployment Laravel

---
+++ heroku-command.php
@@ -0,0 +1,8 @@
+//@ DEPLOY HEROKU CLI - LARAVEL
+//! heroku create
+//! php artisan key:generate --show ///? ini untuk generete new value
+//! heroku config:set APP_KEY={key_value_diatas} ///? setting conf di repo HEROKU
+//! $ git add . // menambhakan file repo ke heroku
+//! $ git commit -m "hello Heroku from Laravel" 
+//!  $ git push heroku master // push
+//! heroku open //? open heroku
